28142a17a962681eedb35cd4db35775c0426440e
Saad Ksioui <saadksioui@gmail.com>
2026-06-09 09:44:27 +0100

    Add TypeScript configuration file (tsconfig.json) for project setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Hi, I'm Saad Ksioui.
+      </h1>
+      <p>I'm a Software Engineer.</p>
+    </div>
+  )
+}
+
+export default Home
